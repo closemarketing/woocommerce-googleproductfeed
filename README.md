23eb22aa19223b8d6f5c9d9b1506d449f85d60c4
David Perez <david@closemarketing.es>
2015-03-20 14:28:20 +0100

    Write to XML Added as a class

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Wordpress Plugin Woocommerce Google Product Feed
 
 This is an alpha version of a puglin that makes a feed for Google Merchant. 
+
+== v0.2 ===
+- Write to XML
+- Added Class
--- woocommerce-googleproductfeed.php
+++ woocommerce-googleproductfeed.php
@@ -4,7 +4,7 @@ Plugin Name: WooCommerce Google Product Feed
 Plugin URI: http://www.closemarketing.es/portafolio/plugin-woocommerce-googleproductfeed/
 Description: Creates a Feed for Google Merchant
 
-Version: 0.1
+Version: 0.2
 Requires at least: 3.0
 
 Author: Closemarketing
@@ -15,87 +15,135 @@ Domain Path: /languages/
 
 License: GPL
 */
-/*
- * localization
- */
-load_plugin_textdomain( 'wc_gfeed', false, basename( dirname(__FILE__) ).'/languages' );
 
-//Google Product Feed
-//Define the product feed php page
-function products_feed_rss2() {
- $rss_template = dirname( __FILE__ ) . '/product-feed.php';
- load_template ( $rss_template );
-}
- 
-//Add the product feed RSS
-add_action('do_feed_products', 'products_feed_rss2', 10, 1);
+class WCGoogleProductFeed {
+    
+    public function __construct()
+    {  
+        //Localization
+        add_action('init', array($this, 'load_plugin_textdomain'));
+        //First XML File
+        add_action('init', array($this, 'generate_xml_product_file'));
+        
+        //Cron
+        add_action( 'wp',  array($this,'prefix_setup_schedule') );
+        add_action( 'prefix_hourly_event', array($this,'prefix_do_this_hourly') );
+        
+        //Taxonomy
+        add_action( 'product_cat_add_form_fields', array($this, 'wc_gfeed_taxonomy_add_new_meta_field'), 10, 2 );
+        add_action( 'product_cat_edit_form_fields', array($this, 'wc_gfeed_taxonomy_edit_meta_field'), 10, 2 );
+        
+        add_action( 'edited_product_cat', array($this, 'save_taxonomy_custom_meta'), 10, 2 );  
+        add_action( 'create_product_cat', array($this, 'save_taxonomy_custom_meta'), 10, 2 );
+    }
+    
+    /**
+     * On an early action hook, check if the hook is scheduled - if not, schedule it.
+     */
+    public function prefix_setup_schedule() 
+    {
+        if ( ! wp_next_scheduled( 'prefix_hourly_event' ) ) {
+            wp_schedule_event( time(), 'hourly', 'prefix_hourly_event');
+        }
+    }
+    /**
+     * On the scheduled action hook, run a function.
+     */
+    public function prefix_do_this_hourly() 
+    {
+        $this->generate_xml_product_file();
+    }
+    
+    /*
+     * localization
+     */
+    public function load_plugin_textdomain()
+    {
+        load_plugin_textdomain('wc_gfeed', FALSE, dirname(plugin_basename(__FILE__)).'/languages/');
+    }
 
-//Update the Rerewrite rules
-add_action('init', 'my_add_product_feed');
- 
-//function to add the rewrite rules
-function my_rewrite_product_rules( $wp_rewrite ) {
- $new_rules = array(
- 'feed/(.+)' => 'index.php?feed='.$wp_rewrite->preg_index(1)
- );
- $wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
-}
- 
-//add the rewrite rule
-function my_add_product_feed( ) {
- global $wp_rewrite;
- add_action('generate_rewrite_rules', 'my_rewrite_product_rules');
- $wp_rewrite->flush_rules();
-}
+    // Add term page
+    public function wc_gfeed_taxonomy_add_new_meta_field() {
+        // this will add the custom meta field to the add new term page
+        ?>
+        <div class="form-field">
+            <label for="term_meta[gcategory]"><?php _e( 'Google Product Category', 'wc_gfeed' ); ?></label>
+            <input type="text" name="term_meta[gcategory]" id="term_meta[gcategory]" value="">
+            <p class="description"><?php _e( 'Enter a value for this field','wc_gfeed' ); ?></p>
+        </div>
+    <?php
+    }
 
+    // Edit term page
+    public function wc_gfeed_taxonomy_edit_meta_field($term) {
 
+        // put the term ID into a variable
+        $t_id = $term->term_id;
 
-// Add term page
-function wc_gfeed_taxonomy_add_new_meta_field() {
-	// this will add the custom meta field to the add new term page
-	?>
-	<div class="form-field">
-		<label for="term_meta[gcategory]"><?php _e( 'Google Product Category', 'wc_gfeed' ); ?></label>
-		<input type="text" name="term_meta[gcategory]" id="term_meta[gcategory]" value="">
-		<p class="description"><?php _e( 'Enter a value for this field','wc_gfeed' ); ?></p>
-	</div>
-<?php
-}
-add_action( 'product_cat_add_form_fields', 'wc_gfeed_taxonomy_add_new_meta_field', 10, 2 );
+        // retrieve the existing value(s) for this meta field. This returns an array
+        $term_meta = get_option( "taxonomy_$t_id" ); ?>
+        <tr class="form-field">
+        <th scope="row" valign="top"><label for="term_meta[gcategory]"><?php _e( 'Google Product Category', 'wc_gfeed' ); ?></label></th>
+            <td>
+                <input type="text" name="term_meta[gcategory]" id="term_meta[gcategory]" value="<?php echo esc_attr( $term_meta['gcategory'] ) ? esc_attr( $term_meta['gcategory'] ) : ''; ?>">
+                <p class="description"><?php _e( 'Enter a value for this field from Table Google Product','wc_gfeed' ); ?> <a href="http://www.google.com/basepages/producttype/taxonomy.<?php echo str_replace("_", "-", WPLANG);?>.txt" target="_blank"><?php _e( 'List File of Google ','wc_gfeed' ); ?></a></p>
+            </td>
+        </tr>
+    <?php
+    }
 
-// Edit term page
-function wc_gfeed_taxonomy_edit_meta_field($term) {
- 
-	// put the term ID into a variable
-	$t_id = $term->term_id;
- 
-	// retrieve the existing value(s) for this meta field. This returns an array
-	$term_meta = get_option( "taxonomy_$t_id" ); ?>
-	<tr class="form-field">
-	<th scope="row" valign="top"><label for="term_meta[gcategory]"><?php _e( 'Google Product Category', 'wc_gfeed' ); ?></label></th>
-		<td>
-			<input type="text" name="term_meta[gcategory]" id="term_meta[gcategory]" value="<?php echo esc_attr( $term_meta['gcategory'] ) ? esc_attr( $term_meta['gcategory'] ) : ''; ?>">
-			<p class="description"><?php _e( 'Enter a value for this field from Table Google Product','wc_gfeed' ); ?> <a href="http://www.google.com/basepages/producttype/taxonomy.<?php echo str_replace("_", "-", WPLANG);?>.txt" target="_blank"><?php _e( 'List File of Google ','wc_gfeed' ); ?></a></p>
-		</td>
-	</tr>
-<?php
-}
-add_action( 'product_cat_edit_form_fields', 'wc_gfeed_taxonomy_edit_meta_field', 10, 2 );
+    // Save extra taxonomy fields callback function.
+    public function save_taxonomy_custom_meta( $term_id ) {
+        if ( isset( $_POST['term_meta'] ) ) {
+            $t_id = $term_id;
+            $term_meta = get_option( "taxonomy_$t_id" );
+            $cat_keys = array_keys( $_POST['term_meta'] );
+            foreach ( $cat_keys as $key ) {
+                if ( isset ( $_POST['term_meta'][$key] ) ) {
+                    $term_meta[$key] = $_POST['term_meta'][$key];
+                }
+            }
+            // Save the option array.
+            update_option( "taxonomy_$t_id", $term_meta );
+        }
+    }
+    
+    /*
+     * Generate XML File
+     */
+    public function generate_xml_product_file() 
+    {
+    
+       $xml = new SimpleXMLElement('<xml/>');
+       $products = get_posts( array( 'post_type'=>'product', 'numberposts'=>-1 ) );
 
-// Save extra taxonomy fields callback function.
-function save_taxonomy_custom_meta( $term_id ) {
-	if ( isset( $_POST['term_meta'] ) ) {
-		$t_id = $term_id;
-		$term_meta = get_option( "taxonomy_$t_id" );
-		$cat_keys = array_keys( $_POST['term_meta'] );
-		foreach ( $cat_keys as $key ) {
-			if ( isset ( $_POST['term_meta'][$key] ) ) {
-				$term_meta[$key] = $_POST['term_meta'][$key];
+       $xml->addChild('item');
+
+       foreach($products as $i=>$product){
+            $name = get_the_title($product->ID);
+            $price = $product->price;
+           
+           /*$terms = get_the_terms( $product->ID, 'product_cat' );
+			foreach ($terms as $term) {
+				$product_cat_id = $term->term_id;
+				break;
 			}
-		}
-		// Save the option array.
-		update_option( "taxonomy_$t_id", $term_meta );
-	}
-}  
-add_action( 'edited_product_cat', 'save_taxonomy_custom_meta', 10, 2 );  
-add_action( 'create_product_cat', 'save_taxonomy_custom_meta', 10, 2 );
+			$term_meta = get_option( "taxonomy_".$product_cat_id );
+			$google_cat = $term_meta['gcategory'];*/
+            $xml->producers->addChild('product');
+            $xml->products->product[$i]->addChild('g:price', $name);
+            $xml->products->product[$i]->addChild('g:image_link', $name);
+            $xml->products->product[$i]->addChild('g:condition', 'new');
+        } 
+        
+        
+        $file = ABSPATH . '/products-feed.xml';
+         
+        $open = fopen($file, 'w') or die ("File cannot be opened.");
+        fwrite($open, $xml->asXML());
+        fclose($open); 
+    }
+    
+} // from Class
+
+$WCGoogleProductFeed = new WCGoogleProductFeed();
